eda724686fbae1c96229afc29d4515f7391c4a4e
Roman Kluth <roman.kluth@outlook.com>
2019-06-19 11:45:04 +0200

    add logic for collision detection

--- scripts.js
+++ scripts.js
@@ -4,7 +4,12 @@ let context = playBox.getContext("2d");
 let x = playBox.width / 2;
 let y = playBox.height - 30;
 
-function ball(){
+let radius = 10;
+
+let dy = -2;
+let dx = 2;
+
+function ball() {
     context.beginPath();
     context.arc(x, y, 10, 0, Math.PI * 2);
     context.fillStyle = "red";
@@ -14,10 +19,20 @@ function ball(){
 
 
 function draw() {
-    context.clearRect(0,0,playBox.width, playBox.height)
+    context.clearRect(0, 0, radius, playBox.width, playBox.height)
     ball();
-    x += 2;
-    y -= 2;
+    x += dx;
+    y += dy;
+    if (x + dx > playBox.width - radius || x + dx < radius) {
+        dx = -dx;
+    }
+    if (y + dy > playBox.height - radius || y + dy < radius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+
 }
 
 setInterval(draw, 10);
